01a6d962c66096069e097fb5512429fa0070702e
josh Long <bean@joshs-MBP.fios-router.home>
2021-03-16 21:28:39 -0400

    afix to averaging route endpoint, now takes url encoded param to match for price averaging

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,30 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { ParsingRoutes }from './routes/parsing';
-import {StatsRoutes} from './routes/statsRoute'
-import {PersistanceManager} from './db';
+
+
+import container from './ioc/ioc_config';
+import { BaseRoute } from './routes/baseRoute';
+import SERVICE_IDENTIFIERS from './identities/identities';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const persistanceManager: PersistanceManager = new PersistanceManager();
-persistanceManager.connect().then(() => {
-    console.info('Instantiating transformation objects....');
-    const parseingRoutes: ParsingRoutes =  new ParsingRoutes(express.Router(), persistanceManager.DB);
-    const statsRoutes: StatsRoutes = new StatsRoutes(express.Router(), persistanceManager.DB);
-    console.info('Done initializing transform objects');
-    
-    console.info('Configuring routes ....');
-    statsRoutes.configureRoutes();
-    parseingRoutes.configureRoutes();
-    
-    app.use(process.env.BASE_API_ROUTE || '/api', parseingRoutes.Router);
-    app.use(process.env.BASE_API_ROUTE || '/api', statsRoutes.Router);
-    
-    app.listen(process.env.API_PORT);
-    console.log('api listening on', process.env.API_PORT);    
-});
+
+const parsingRoute: BaseRoute 
+    = container.getNamed(SERVICE_IDENTIFIERS.ROUTES,'parsing');
+const averagingRouote: BaseRoute = 
+    container.getNamed(SERVICE_IDENTIFIERS.ROUTES,'stats');
+
+parsingRoute.initializeRouter(express.Router());
+averagingRouote.initializeRouter(express.Router());
+
+parsingRoute.configureRoutes();
+averagingRouote.configureRoutes();
+
+
+app.use(process.env.BASE_API_ROUTE || '/api', parsingRoute.Router);
+app.use(process.env.BASE_API_ROUTE || '/api', averagingRouote.Router);
+
+app.listen(process.env.API_PORT);
+console.log('api listening on', process.env.API_PORT);
