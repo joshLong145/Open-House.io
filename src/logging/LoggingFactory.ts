e76936d7215acb171734e14e8aa7052d72bb850d
josh long <jlong2@ramapo.edu>
2021-12-02 23:26:13 -0500

    Ci/container runner (#21)
    * ci: addition  of test runner action
    
    * ci: moving of gh action
    
    * ref: refactoring
    
    * dev: fixes to transformers
    
    * dev: fix to duplication in adc transformer
    
    * dev: addition of logging using winston
    
    * dev: addition of logging using winston
    
    * fix: fix to type mismatch in parsing.ts
    
    * fix: fix to result type error
    
    * ci: addition of container build action
    
    * dev: addition of action name
    
    * ew
    
    * ew
    
    * hack for runner
    
    * logger injection start
    
    * dev: update to base class for transformers to inject logger
    
    * dev: more logging
    
    * ci: update to docker pipeline
    
    * ci: addition of creds for push step
    
    * ci: update to action naming
    
    * dev: addition of test ts configuration
    
    Co-authored-by: a a <a@a.a>

--- src/logging/LoggingFactory.ts
+++ src/logging/LoggingFactory.ts
@@ -1,17 +1,33 @@
 
 export const GenerateConsoleLogger = function(name: string)
 {
-    const { createLogger, format, transports } = require('winston');
-    const { combine, timestamp, label, splat, simple } = format;
+    const { addColors, createLogger, format, transports } = require('winston');
+    const { combine, timestamp, label, splat, simple, colorize } = format;
+
+    addColors({
+      warn: 'yellow',
+      info: 'blue',
+      debug: 'green'
+    });
 
     return createLogger({
         format: combine(
           label({ label: name }),
           splat(),
           simple(),
-          timestamp()
+          timestamp(),
+          colorize()
         ),
-        transports: [new transports.Console()]
+        transports: [
+          new(transports.Console)({
+            level: 'debug',
+            colorize: true,
+            timestamp: function () {
+                return (new Date()).toLocaleTimeString();
+            },
+            prettyPrint: true
+          })
+        ]
       });
 }
 
